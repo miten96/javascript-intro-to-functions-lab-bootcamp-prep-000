1f67c56380ca87bafb665ee39d51f2f10cf452af
Miten Kashiyani <miten.kashiyani96@gmail.com>
2020-05-23 16:00:28 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ function logWhisper(string) {
   console.log(string.toLowerCase())
 }
 
-function sayHiToGrandma('string') {
+function sayHiToGrandma(string) {
   var lowerCase = 'hello'
 
   if (string === lowerCase) {
